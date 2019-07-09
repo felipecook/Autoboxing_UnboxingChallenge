1dfdcc6c3e5e22e2074998928836e61e26bcbb51
Felipe Cook <felipepcook@gmail.com>
2019-07-08 20:05:02 -0700

    Initial commit

---
+++ src/Bank.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Bank {
+
+  ArrayList<Branches> listOfBranches = new ArrayList<Branches>();
+
+  public class Branches {
+    ArrayList<Customers> enrolledCustomers = new ArrayList<>();
+
+
+  }
+
+}
