a14e4f6918c6e80cb9a59882b8ce5044cb536f7f
Felipe Cook <felipepcook@gmail.com>
2019-07-10 19:45:23 -0700

    Completed BankingBranch class, next is work on Bank

--- src/Bank.java
+++ src/Bank.java
@@ -5,9 +5,8 @@ public class Bank {
   private ArrayList<BankingBranch> listOfBranches = new ArrayList<BankingBranch>();
 
   public void addNewBranch (String branchName) {
-    BankingBranch bankingBranch = new BankingBranch(branchName);
 
-    listOfBranches.add(bankingBranch);
+
   }
 
 
--- src/BankingBranch.java
+++ src/BankingBranch.java
@@ -2,15 +2,43 @@ import java.util.ArrayList;
 
 public class BankingBranch {
   private final String branchName;
-  private ArrayList<Customer> enrolledCustomers = new ArrayList<>();
+  private ArrayList<Customer> enrolledCustomers;
 
   BankingBranch(String branchName) {
     this.branchName = branchName;
+    this.enrolledCustomers = new ArrayList<Customer>();
   }
 
-  public void addCustomer (String name, Double initialAmount) {
-    Customer customer = new Customer(name, initialAmount);
-    // Needs a check to see about if the customer is already enrolled
-    enrolledCustomers.add(customer);
+  public boolean addCustomer (String name, Double initialAmount) {
+    if (findCustomer(name) == null) {
+      this.enrolledCustomers.add(new Customer(name, initialAmount));
+      return true;
+    }
+    return false;
+  }
+
+  public boolean addCustomerTransaction (String customerName, Double amount) {
+    Customer existingCustomer = findCustomer(customerName);
+
+    if (existingCustomer != null) {
+      existingCustomer.addTransaction(amount);
+      return true;
+    }
+    return false;
+  }
+
+  private Customer findCustomer(String customerName) {
+
+    for (Customer enrolledCustomer : this.enrolledCustomers) {
+      if (enrolledCustomer.getName().equals(customerName)) {
+        return enrolledCustomer;
+      }
+    }
+
+    return null;
+  }
+
+  public String getBranchName() {
+    return branchName;
   }
 }
